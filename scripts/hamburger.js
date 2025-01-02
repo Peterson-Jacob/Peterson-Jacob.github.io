11fb2a4292d95622fd2f53427cc188e8fcb6d2f4
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2025-01-02 15:05:47 -0600

    Update hamburger.js

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,4 +1,24 @@
- function toggle(nav) {
-    nav.classList.toggle('show'); 
+
+window.onload = function(){
+
+
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('nav-menu');
+    const navLinks = document.querySelectorAll('.nav-menu li a');
     
+    hamburger.addEventListener('click', function() {
+        navMenu.classList.toggle('open');
+        hamburger.classList.toggle('open');
+
+
+    });
+  
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() { 
+            navMenu.classList.remove('open'); 
+            hamburger.classList.remove('open'); 
+        });
+     });
+
 }
+
